34f4e8894e49fa7b6f2a902a97172f1cdc10911f
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-03-06 18:10:18 -0400

    perf: reparado as questões solicitadas na PR #2

--- Domain/DTO/Cliente/ClienteDTO.cs
+++ Domain/DTO/Cliente/ClienteDTO.cs
@@ -7,16 +7,16 @@ namespace safi_gerenciamento_xml.Domain.DTO.Cliente
 {
     public class ClienteDTO
     {
-        public Guid? Id_Cliente {get;}
-        public string? Nome_Cliente {get; set;}
-        public string? Email_Cliente {get; set;}
-        public Guid? Id_Contador {get; set;}
+        public Guid id_Cliente {get;}
+        public string? nome_Cliente {get; set;}
+        public string? email_Cliente {get; set;}
+        public Guid id_Contador {get; set;}
 
         public ClienteDTO(string nomeCliente, string emailCliente, Guid idContador)
         {
-            Nome_Cliente = nomeCliente;
-            Email_Cliente = emailCliente;
-            Id_Contador = idContador;
+            nome_Cliente = nomeCliente;
+            email_Cliente = emailCliente;
+            id_Contador = idContador;
         }
     }
 }
--- Domain/DTO/Contador/ContadorDTO.cs
+++ Domain/DTO/Contador/ContadorDTO.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Cassandra.Data.Linq;
 
 namespace safi_gerenciamento_xml.Domain.DTO.Contador
 {
     public class ContadorDTO
     {
-        [Key]
-        public Guid? Id_Contador {get;}
-        public string? Nome_Contador {get; set;}
-        public string? Email_Contador {get; set;}
+        public Guid id_Contador {get;}
+        public string? nome_Contador {get; set;}
+        public string? email_Contador {get; set;}
 
         public ContadorDTO(string nomeContador, string emailContador)
         {
-            Nome_Contador = nomeContador;
-            Email_Contador = emailContador;
+            nome_Contador = nomeContador;
+            email_Contador = emailContador;
         }
     }
 }
